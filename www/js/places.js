8d5264bb4157e412aaf6bed918060e97a4298295
kkdaemen <kkdaemen@avanas.nl>
2016-04-02 21:57:44 +0200

    js fix

--- www/js/places.js
+++ www/js/places.js
@@ -1,73 +1,81 @@
 
 
-$(document).on('pageinit', '#home', function(){      
+document.addEventListener("deviceready", onDeviceReady, false);
+
+ function onDeviceReady() {
+
+    $(document).on('pageinit', '#indebuurt', function(){      
     var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?',
     radius = '&radius=5000',
     location = '&location=51.788394,%205.848014', 
     type= '&type=cafe',        
     key = 'key=AIzaSyDZHWVrdN6pma0WKoAVhV2zEwHywXETnh0';        
     
-    $.ajax({
-        url: url + key + location + radius + type ,
-        dataType: "json",
-        async: true,
-        success: function (result) {
-           kroegInfo.result = result.results;
+        $.ajax({
+            url: url + key + location + radius + type ,
+            dataType: "json",
+            async: true,
+            success: function (result) {
+               kroegInfo.result = result.results;
 
-           $.each(result.results, function(i, row) {
-                //console.log(JSON.stringify(row)); 
-                var icon = row.icon;
-                
-                if (row.hasOwnProperty('photos')) {
-                    console.log(imgUrl.url + imgUrl + row.photos[0].photo_reference + imgUrl.key); 
-                    icon = imgUrl.url + row.photos[0].photo_reference + imgUrl.key; 
-                };
-                $('#kroeg-list').append('<li><a href="" data-id="' + row.id + '"><img src="'+icon+'"/><h3>' + row.name + '</h3><p>' + row.rating + '<i class="fa fa-star"></i></p></a></li>');  
-            });
-            $('#kroeg-list').listview('refresh');
-       },
-       error: function (request,error) {
-            //alert('Network error has occurred please try again!');
-        }
-    });         
-});
+               $.each(result.results, function(i, row) {
+                    //console.log(JSON.stringify(row)); 
+                    var icon = row.icon;
+                    
+                    if (row.hasOwnProperty('photos')) {
+                        console.log(imgUrl.url + imgUrl + row.photos[0].photo_reference + imgUrl.key); 
+                        icon = imgUrl.url + row.photos[0].photo_reference + imgUrl.key; 
+                    };
+                    $('#kroeg-list').append('<li><a href="" data-id="' + row.id + '"><img src="'+icon+'"/><h3>' + row.name + '</h3><p>' + row.rating + '<i class="fa fa-star"></i></p></a></li>');  
+                });
+                $('#kroeg-list').listview('refresh');
+           },
+           error: function (request,error) {
+                console.log('Network error has occurred please try again!');
+            }
+        });         
+    });
 
-$(document).on('pagebeforeshow', '#headline', function(){   
-   
-    $('#kroeg-data').empty();
-    $.each(kroegInfo.result, function(i, row) {
-        
-        if(row.id == kroegInfo.id) {
+    $(document).on('pagebeforeshow', '#headline', function(){   
+       
+        $('#kroeg-data').empty();
+        $.each(kroegInfo.result, function(i, row) {
             
-            var icon = row.icon;
-                console.log(JSON.stringify(row));
-                if (row.hasOwnProperty('photos')) {
-                    console.log(imgUrl.url + imgUrl + row.photos[0].photo_reference + imgUrl.key);    
-                    icon = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=1600&photoreference=' + row.photos[0].photo_reference + imgUrl.key; 
-                };
-            $('#kroeg-data').append('<li><img src="'+icon+'"></li>');
-            $('#kroeg-data').append('<li>Naam: '+row.name+'</li>');
-            $('#kroeg-data').append('<li>Beoordeling: '+row.rating+'</li>');
-            $('#kroeg-data').append('<li>Adres: '+row.vicinity+'</li>');   
-            $('#kroeg-data').append('<li>place_id : '+row.place_id+'</li>');             
-            $('#kroeg-data').listview('refresh');              
-        }
-    });    
-});
+            if(row.id == kroegInfo.id) {
+                
+                var icon = row.icon;
+                    console.log(JSON.stringify(row));
+                    if (row.hasOwnProperty('photos')) {
+                        console.log(imgUrl.url + imgUrl + row.photos[0].photo_reference + imgUrl.key);    
+                        icon = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=1600&photoreference=' + row.photos[0].photo_reference + imgUrl.key; 
+                    };
+                $('#kroeg-data').append('<li><img src="'+icon+'"></li>');
+                $('#kroeg-data').append('<li>Naam: '+row.name+'</li>');
+                $('#kroeg-data').append('<li>Beoordeling: '+row.rating+'</li>');
+                $('#kroeg-data').append('<li>Adres: '+row.vicinity+'</li>');   
+                $('#kroeg-data').append('<li>place_id : '+row.place_id+'</li>');             
+                $('#kroeg-data').listview('refresh');              
+            }
+        });    
+    });
 
-$(document).on('vclick', '#kroeg-list li a', function(){  
+    $(document).on('vclick', '#kroeg-list li a', function(){  
 
-    kroegInfo.id = $(this).attr('data-id');
-    $.mobile.changePage( "#headline", { transition: "slide", changeHash: false });
-});
+        kroegInfo.id = $(this).attr('data-id');
+        $.mobile.changePage( "#headline", { transition: "slide", changeHash: false });
+    });
 
-var imgUrl = {
-    url : 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=',
-    key : '&key=AIzaSyDZHWVrdN6pma0WKoAVhV2zEwHywXETnh0'
-}
+    var imgUrl = {
+        url : 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=',
+        key : '&key=AIzaSyDZHWVrdN6pma0WKoAVhV2zEwHywXETnh0'
+    }
 
-var kroegInfo = {
-    id : null,
-    result : null
+    var kroegInfo = {
+        id : null,
+        result : null
+    } 
+
+
+        
 }
 
