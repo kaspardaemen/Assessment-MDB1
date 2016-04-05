f605bd112fdcf1dde74d5d4bf586c55dfb212c72
TobyWanKanobi <t.p.maipauw@gmail.com>
2016-04-05 04:20:22 +0200

    GMAP

--- www/js/maps.js
+++ www/js/maps.js
@@ -1,15 +1,51 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
- function onDeviceReady() {
+function onDeviceReady() {
 
-    //krijg de details van een café
-    $(document).on('vclick', '#pokemap', function(){  
+    // 
+    $(document).on('vclick', '#pokemap-nav', function(){  
     	console.log('Je moeder is je oma');
-    	$.mobile.changePage( "#pokemap", { transition: "slide", changeHash: false });
+    	$.mobile.pageContainer.pagecontainer('change', '../map.html', { role: "page" } );
     });
+    
+    /*
+    * Google Maps documentation: http://code.google.com/apis/maps/documentation/javascript/basics.html
+    * Geolocation documentation: http://dev.w3.org/geo/api/spec-source.html
+    */
 
-
-    $.mobile.pageContainer.pagecontainer('load', '../map.html');    
+    $( document ).on( "pageinit", "#pokemap", function() {
+    	console.log("POKEmap paginit");
+    	var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
+    
+    /*if ( navigator.geolocation ) {
+        function success(pos) {
+            // Location found, show map with these coordinates
+            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        }
+        function fail(error) {
+            drawMap(defaultLatLng);  // Failed to find location, show default map
+        }
+        // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
+        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+    } else {
+        drawMap(defaultLatLng);  // No geolocation support, show default map
+    }*/
+		function drawMap(latlng) {
+	        var myOptions = {
+	            zoom: 10,
+	            center: defaultLatLng,
+	            mapTypeId: google.maps.MapTypeId.ROADMAP
+	        };
+        	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+        // Add an overlay to the map of current lat/lng
+        /*var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: "Greetings!"
+        });*/
+		}
+	});
+    
 }
 
 /*console.log('run MAP.js');
