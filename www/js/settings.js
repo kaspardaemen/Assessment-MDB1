12c4c204836074b48d52fb322f24c4bb8cfb55c2
kkdaemen <kkdaemen@avanas.nl>
2016-04-05 23:06:00 +0200

    settings

---
+++ www/js/settings.js
@@ -0,0 +1,30 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
+ function onDeviceReady() {
+
+ 	//apply theme
+ 	//console.log(window.localStorage.getItem('theme'));
+
+ 	var theme = null;
+ 		if(window.localStorage.getItem('theme') === null){
+    		$(".page").attr("data-theme", "b"); 
+    		$("#home").attr("data-theme", "b"); 
+   		}else {
+   			$(".page").attr("data-theme", window.localStorage.getItem('theme'));   
+   			$("#home").attr("data-theme", window.localStorage.getItem('theme')); 
+   		}
+   
+    $(document).on('vclick', '#changeTheme', function(){  
+
+ 			var storage = window.localStorage; 
+ 			console.log($('#themeswitch').val());
+	      	storage.setItem('theme', $('#themeswitch').val());     
+	      	console.log(window.localStorage.getItem('theme'));
+
+	    });
+};
+
+
+	
+
+ 	
