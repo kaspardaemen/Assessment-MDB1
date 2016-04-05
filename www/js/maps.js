57d4d7268b85d270046c39d5e1a4c9a5311a5084
TobyWanKanobi <t.p.maipauw@gmail.com>
2016-04-05 17:17:22 +0200

    Map

--- www/js/maps.js
+++ www/js/maps.js
@@ -2,10 +2,15 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
 
+    $('body').load('map.html', function (result) {
+        $('body').append(result);
+   // $(this).trigger('create');
+});
     // 
-    $(document).on('vclick', '#pokemap-nav', function(){  
+    $(document).on('vclick', '#nav-map', function(){  
     	console.log('Je moeder is je oma');
-    	$.mobile.pageContainer.pagecontainer('change', '../map.html', { role: "page" } );
+       // $.mobile.pageContainer.pagecontainer('load', '../map.html', { } );
+    	//$.mobile.pageContainer.pagecontainer('change', '#pokemap', {} );
     });
     
     /*
@@ -13,108 +18,50 @@ function onDeviceReady() {
     * Geolocation documentation: http://dev.w3.org/geo/api/spec-source.html
     */
 
-    $( document ).on( "pageinit", "#pokemap", function() {
+    $.mobile.pageContainer.on( "load", "#pokemap", function() {
     	console.log("POKEmap paginit");
-    	var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
-    
-    /*if ( navigator.geolocation ) {
-        function success(pos) {
-            // Location found, show map with these coordinates
-            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-        }
-        function fail(error) {
-            drawMap(defaultLatLng);  // Failed to find location, show default map
-        }
-        // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
-        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
-    } else {
-        drawMap(defaultLatLng);  // No geolocation support, show default map
-    }*/
-		function drawMap(latlng) {
-	        var myOptions = {
-	            zoom: 10,
-	            center: defaultLatLng,
-	            mapTypeId: google.maps.MapTypeId.ROADMAP
-	        };
-        	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+    	CreateMap();
+	});
+
+	function CreateMap(){
+
+        console.log('CreateMap');
+		var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
+
+		if ( navigator.geolocation ) {
+			function success(pos) {
+                console.log('Using navigator.geolocation' + pos.coords.latitude);
+				// Location found, show map with these coordinates
+            	drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        	}
+        	function fail(error) {
+            	drawMap(defaultLatLng);  // Failed to find location, show default map
+        	}
+        	
+        	// Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
+        	navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+    	} else {
+        	drawMap(defaultLatLng);  // No geolocation support, show default map
+    	}
+	}
+
+	function drawMap(latlng) {
+		
+        console.log(latlng);
+		var myOptions = {
+			zoom: 10,
+			center: latlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+	       
+        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+        
         // Add an overlay to the map of current lat/lng
-        /*var marker = new google.maps.Marker({
+        var marker = new google.maps.Marker({
             position: latlng,
             map: map,
             title: "Greetings!"
-        });*/
-		}
-	});
+        });
+	}
     
 }
-
-/*console.log('run MAP.js');
-
-document.addEventListener("deviceready", map, false);
-
-function map() {
-
-	$(document).on('pageinit', '#pokemap', function(){ 
-		console.log('POKEMAP');
-	});
-
-	/*console.log('MAP onDeviceReady');
-	$(document).on("loadmap", function(){
-		console.log('loadmap page!!!!');
-		$.mobile.loadPage("../map.html");
-		alert('dsfsdf');
-	});*/
-//});
-
-/*
-function initMap() {
-    var mapDiv = document.getElementById('pokemap');
-    var map = new google.maps.Map(mapDiv);
-};
-
-
-document.addEventListener("deviceready", onDeviceReady, false);
-
- function onDeviceReady() {
- 	$(document).ready(function(){
-
-
- 	});
-
-	$( document ).on( "pageinit", "#map-page", function() {
-
-
-
-
-
-	    var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
-	    if ( navigator.geolocation ) {
-	        function success(pos) {
-	            // Location found, show map with these coordinates
-	            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-	        }
-	        function fail(error) {
-	            drawMap(defaultLatLng);  // Failed to find location, show default map
-	        }
-	        // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
-	        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
-	    } else {
-	        drawMap(defaultLatLng);  // No geolocation support, show default map
-	    }
-	    function drawMap(latlng) {
-	        var myOptions = {
-	            zoom: 10,
-	            center: latlng,
-	            mapTypeId: google.maps.MapTypeId.ROADMAP
-	        };
-	        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-	        // Add an overlay to the map of current lat/lng
-	        var marker = new google.maps.Marker({
-	            position: latlng,
-	            map: map,
-	            title: "Hier ben ik!"
-	        });
-	    }
-	});
-
- }*/
