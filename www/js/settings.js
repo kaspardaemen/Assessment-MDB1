9c4e1bf4b4435d5df557a9bff65dac59eb0c119c
TobyWanKanobi <t.p.maipauw@gmail.com>
2016-04-25 22:37:23 +0200

    switch themes fixed

--- www/js/settings.js
+++ www/js/settings.js
@@ -1,37 +1,44 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
- function onDeviceReady() {
+function onDeviceReady() {
 
- 	//apply theme
- 	//console.log(window.localStorage.getItem('theme'));
+  var defaultTheme = 'a';
 
- 	var theme = null;
- 		if(window.localStorage.getItem('theme') === null){
-    		$(".page").attr("data-theme", "b"); 
-    		$("#home").attr("data-theme", "b"); 
-    		$("themeswitch").val("b");    
-   		}else {
-   			$(".page").attr("data-theme", window.localStorage.getItem('theme'));   
-   			$("#home").attr("data-theme", window.localStorage.getItem('theme')); 
-   			$("themeswitch").val( window.localStorage.getItem('theme'));     
-   		}
-   
-    $(document).on('vclick', '#changeTheme', function(){  
+  if(window.localStorage.getItem('theme') === null){
+    window.localStorage.setItem('theme', defaultTheme);  
+  }
 
- 			var storage = window.localStorage; 
- 			console.log($('#themeswitch').val());
-	      	storage.setItem('theme', $('#themeswitch').val());     
-	      	console.log(window.localStorage.getItem('theme'));
+  var currentTheme = window.localStorage.getItem('theme');
 
-	    });
+  // Apply themes on startup (home)
+  $('[data-role="page"]').removeClass('ui-page-theme-a ui-page-theme-b').addClass('ui-page-theme-' + currentTheme);
 
-    $(document).on('vclick', '#clearcache', function(){
-      console.log('clear cache');
-      window.localStorage.removeItem('pokemon');
-    });
+  /* Apply theme newly created pages */
+  $(document).on('pagebeforecreate', function(event){
+
+    console.log('Apply theme: ' + currentTheme);
+    $(event.target).removeClass('ui-page-theme-a ui-page-theme-b').addClass('ui-page-theme-' + currentTheme);
+
+  });
+
+  /* Store theme in localstorage */
+  $(document).on('vclick', '#changeTheme', function(){
+    
+    window.localStorage.setItem('theme', $('#themeswitch').val());
+    currentTheme = window.localStorage.getItem('theme');
+
+    /* Apply theme after new theme is selected and saved */
+    $('[data-role="page"]').removeClass('ui-page-theme-a ui-page-theme-b').addClass('ui-page-theme-' + currentTheme);
+
+  });
+
+  $(document).on('vclick', '#clearcache', function(){
+    window.localStorage.removeItem('pokemon');
+  });
+
+  $(document).on('pagebeforecreate', '#settings-page', function(event){
+
+    $('#themeswitch option[value="' + currentTheme + '"]').attr('selected', true);
+
+  });
 };
-
-
-	
-
- 	
