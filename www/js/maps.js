63a2cf79cb81a53f07e3df634038d1e7b8161b00
kkdaemen <kkdaemen@avanas.nl>
2016-05-12 22:42:50 +0200

    ..

--- www/js/maps.js
+++ www/js/maps.js
@@ -44,7 +44,27 @@ function onDeviceReady() {
 
     $(document).on('pageshow', '#wilderness-page', function(){
         console.log('Wilderness page');
-        GenerateSpawnPoint();
+        
+        var myOptions = {
+                zoom: 16,
+                center: new google.maps.LatLng(51.687968, 5.286327),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+               
+            var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+
+            // Construct the polygon.
+            var denBoschPolygon = new google.maps.Polygon({
+                paths: denBoschCoords,
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#FF0000',
+                fillOpacity: 0.10
+            });
+
+            denBoschPolygon.setMap(map);
+            GenerateSpawnPoint(map, denBoschPolygon);
 
         var randomPokemon = function(){
 
